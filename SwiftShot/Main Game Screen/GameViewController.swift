d496b75586f1efde55e54011bdba2a11b32a19e0
XanderXu <517807635@qq.com>
2018-09-11 17:37:41 +0800

    更新翻译

--- SwiftShot/Main Game Screen/GameViewController.swift
+++ SwiftShot/Main Game Screen/GameViewController.swift
@@ -205,6 +205,9 @@ class GameViewController: UIViewController {
         // hierarchy of the scene, so it isn't affected by changes to the scene scale.
         // On each frame update, however, its position is explicitly set to a transformed
         // value that is consistent with the game objects in the scene.
+        // 将音频听者的位置设置到一个我们控制的SCNNode上
+        // 因为渲染坐标空间的缩放,与物理模拟坐标空间的缩放不同.这个节点并没有添加到场景树的层级节点上,所以它不会被场景缩放影响.
+        // 在每个帧更新时,它的位置被设置为了个变换过的值,这个值和场景中的游戏物体保持一致.
         sceneView.audioListener = audioListenerNode
 
         sceneView.scene.rootNode.addChildNode(gameBoard)
@@ -234,6 +237,7 @@ class GameViewController: UIViewController {
                                                object: nil)
         
         // this preloads the assets used by the level - materials and texture and compiles shaders
+        // 加载各个级别中用到的素材--材质,纹理和着色器
         preloadLevel()
     }
 
@@ -247,6 +251,7 @@ class GameViewController: UIViewController {
     private func updateThermalStateIndicator() {
         DispatchQueue.main.async {
             // Show thermal state label if default enabled and state is critical
+            // 当显示热状态label
             self.thermalStateLabel.isHidden = !(UserDefaults.standard.showThermalState && ProcessInfo.processInfo.thermalState == .critical)
         }
     }
@@ -258,17 +263,21 @@ class GameViewController: UIViewController {
         var debugOptions: SCNDebugOptions = []
         
         // fix the scaling of the physics debug view to match the world
+        // 修复缩放导致的物理视图bug,以匹配世界
         if UserDefaults.standard.showPhysicsDebug { debugOptions.insert(.showPhysicsShapes) }
         
         // show where ARKit is detecting feature points
+        // 显示ARKit检测到的特征点
         if UserDefaults.standard.showARDebug { debugOptions.insert(ARSCNDebugOptions.showFeaturePoints) }
         
         // see high poly-count and LOD transitions - wireframe overlay
+        // 显示多边形数目和细节级别
         if UserDefaults.standard.showWireframe { debugOptions.insert(SCNDebugOptions.showWireframe) }
         
         sceneView.debugOptions = debugOptions
         
         // perf stats
+        // 性能统计
         sceneView.showsStatistics = UserDefaults.standard.showSceneViewStats
         
         trackingStateLabel.isHidden = !UserDefaults.standard.showTrackingState
@@ -276,6 +285,8 @@ class GameViewController: UIViewController {
         // smooth the edges by rendering at higher resolution
         // defaults to none on iOS, use on faster GPUs
         // 0, 2, 4 on iOS, 8, 16x on macOS
+        // 提高分辨率进行边缘反锯齿
+        // iOS上默认为none,用在更新更快的GPU上,0, 2, 4 on iOS, 8, 16x on macOS
         sceneView.antialiasingMode = UserDefaults.standard.antialiasingMode ? .multisampling4X : .none
         
         os_log(.info, "antialiasing set to: %s", UserDefaults.standard.antialiasingMode ? "4x" : "none")
@@ -322,16 +333,19 @@ class GameViewController: UIViewController {
             return
         case .lookingForSurface, .waitingForBoard:
             // both server and client, go ahead and start tracking the world
+            // 主机与从机,均开始追踪世界
             configuration.planeDetection = [.horizontal]
             options = [.resetTracking, .removeExistingAnchors]
             
             // Only reset session if not already running
+            // 只有在已经不运行的情况下,才重置session
             if sceneView.isPlaying {
                 return
             }
         case .placingBoard, .adjustingBoard:
             // we've found at least one surface, but should keep looking.
             // so no change to the running session
+            // 我们已经发现了至少一个平面,但应该继续寻找.所以不要改变运行中的session
             return
         case .localizingToBoard:
             guard let targetWorldMap = targetWorldMap else { os_log(.error, "should have had a world map"); return }
@@ -350,10 +364,12 @@ class GameViewController: UIViewController {
             return
         case .gameInProgress:
             // The game is in progress, no change to the running session
+            // 游戏正在进行中,不改变运行中的session
             return
         }
         
         // Turning light estimation off to test PBR on SceneKit file
+        // 关闭光照估计来测试PBR在SceneKit文件上的效果
         configuration.isLightEstimationEnabled = false
         
         os_log(.info, "configured AR session")
@@ -365,6 +381,7 @@ class GameViewController: UIViewController {
         let leaveAction = UIAlertAction(title: NSLocalizedString("Leave", comment: ""), style: .cancel) { _ in
             self.exitGame()
             // start looking for beacons again
+            // 重新开始寻找beacons
             self.proximityManager.start()
         }
         let stayAction = UIAlertAction(title: NSLocalizedString("Stay", comment: ""), style: .default)
@@ -387,6 +404,7 @@ class GameViewController: UIViewController {
         showOverlay()
         
         // Cleanup the current loaded map
+        // 清理当前加载过的地图
         targetWorldMap = nil
         teamACatapultImages.forEach {
             $0.isHidden = true
@@ -401,6 +419,7 @@ class GameViewController: UIViewController {
         UserDefaults.standard.hasOnboarded = false
         
         // Reset game board
+        // 重置游戏的底座
         gameBoard.reset()
         if let boardAnchor = gameBoard.anchor {
             sceneView.session.remove(anchor: boardAnchor)
@@ -428,26 +447,30 @@ class GameViewController: UIViewController {
         
         if sessionState == .setupLevel {
             // this will advance the session state
+            // 提升session状态
             setupLevel()
             return
         }
         
         // Only automatically update board when looking for surface or placing board
+        // 只在寻找表面或放置底座过程中,才自动调整底座
         guard attemptingBoardPlacement else {
             return
         }
         
         // Make sure this is only run on the render thread
-        
+        // 确保只在渲染线程运行
         if gameBoard.parent == nil {
             sceneView.scene.rootNode.addChildNode(gameBoard)
         }
         
         // Perform hit testing only when ARKit tracking is in a good state.
+        // 只有在ARKit追踪状态正常时,才执行命中测试
         if case .normal = frame.camera.trackingState {
             
             if let result = sceneView.hitTest(screenCenter, types: [.estimatedHorizontalPlane, .existingPlaneUsingExtent]).first {
                 // Ignore results that are too close to the camera when initially placing
+                // 当初始化放置时,忽略那些太靠近相机的点
                 guard result.distance > 0.5 || sessionState == .placingBoard else { return }
                 
                 sessionState = .placingBoard
@@ -478,6 +501,7 @@ class GameViewController: UIViewController {
     }
     
     /// Load the World Map from archived data
+    /// 从压缩数据中加载出世界地图
     func loadWorldMap(from archivedData: Data) {
         do {
             let uncompressedData = try archivedData.decompressed()
@@ -513,8 +537,10 @@ class GameViewController: UIViewController {
             for level in GameLevel.allLevels {
                 // this is just a dummy scene to preload data
                 // this is not added to the sceneView
+                // 这只是个场景的替身来预加载数据,它不会被加载到sceneView上
                 let scene = SCNScene()
                 // this may result in two callers loading the scene
+                // 这里会导致两个调用者加载场景
                 level.load()
                 if let activeLevel = level.activeLevel {
                     self.setLevelLighting(activeLevel)
@@ -525,8 +551,12 @@ class GameViewController: UIViewController {
                     // the preloading is done on a background thread, and the completion handler called
 
                     // prepare must be called from main thread
+                    // 这里并不会真的将场景添加到ARSCNView上,它只是建立一个后台任务
+                    // 预加载在后台线程完成,并调用completion handler
+                    // 预加载必需是在主线程被调用
                     main.sync {
                         // preparing a scene compiles shaders
+                        // 准备一个场景来编译着色器
                         self.sceneView.prepare([scene], completionHandler: { success in
                             if success {
                                 os_signpost(.end, log: .preload_assets, name: .preload_assets, signpostID: .preload_assets,
@@ -578,6 +608,7 @@ class GameViewController: UIViewController {
         }
 
         // stop ranging for beacons after placing board
+        // 在放置过底座后,停止监听beacons
         if UserDefaults.standard.gameRoomMode {
             proximityManager.stop()
             if let location = proximityManager.closestLocation {
@@ -686,9 +717,11 @@ extension GameViewController: SCNSceneRendererDelegate {
             if let pointOfView = sceneView.pointOfView, let level = selectedLevel, level.placed {
                 // make a copy of the camera data that other threads can access
                 // ARKit has updated the transform right before this
+                // 把相机数据复制一份,这样其他线程也能访问该数据.ARKit在此之前已经更新过了transform
                 gameManager.copySimulationCamera()
                 
                 // these can use the pointOfView since the render thread scales/unscales the camera around rendering
+                // 这里可以使用pointOfView,因为渲染线程在渲染前后缩小/放大了相机
                 let cameraTransform = gameManager.renderSpaceTransformToSimulationSpace(transform: pointOfView.simdTransform)
                 let cameraInfo = CameraInfo(transform: cameraTransform)
                 
@@ -728,15 +761,18 @@ extension GameViewController: SCNSceneRendererDelegate {
                     "Post constraints update started")
         if let gameManager = gameManager, gameManager.isInitialized {
             // scale up/down the camera to render space
+            // 缩小/放大相机到渲染空间
             gameManager.scaleCameraToRender()
             
             // render space from here until scaleCameraToSimulation() is called
+            // 渲染空间,从这里直到scaleCameraToSimulation()被调用
             if let pointOfView = sceneView.pointOfView {
                 audioListenerNode.simdWorldTransform = pointOfView.simdWorldTransform
             }
             
             // The only functionality currently controlled here is the trail on the projectile.
             // Therefore this part is used to turn on/off show projectile trail
+            // 这里控制的功能是是否显示弹道尾迹
             if UserDefaults.standard.showProjectileTrail {
                 gameManager.onDidApplyConstraints(renderer: renderer)
             }
@@ -753,14 +789,17 @@ extension GameViewController: SCNSceneRendererDelegate {
 
     func renderer(_ renderer: SCNSceneRenderer, didRenderScene scene: SCNScene, atTime time: TimeInterval) {
         // update visibility properties in renderloop because we have to scale the physics world down to render properly
+        // 更新渲染循环中的可见性属性,因为我们必须缩放物理世界到渲染属性
         if let gameManager = gameManager, gameManager.isInitialized {
         
             // this visibility test is in scaled space, using renderer frustum culling
+            // 这个可见性测试是在缩放过的空间的,使用渲染器平头截体剔除
             if let pointOfView = sceneView.pointOfView {
                 gameManager.updateCatapultVisibility(renderer: renderer, camera: pointOfView)
             }
             
             // return the pointOfView back from scaled space
+            // 从缩放过的空间中返回pointOfView
             gameManager.scaleCameraToSimulation()
         }
 
@@ -785,12 +824,15 @@ extension GameViewController: ARSessionDelegate {
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         
         // Update game board placement in physical world
+        // 更新游戏底座在物理世界的位置
         if gameManager != nil {
             // this is main thread calling into init code
+            // 主线程调用
             updateGameBoard(frame: frame)
         }
         
         // Update mapping status for saving maps
+        // 更新地图状态,以供保存
         updateMappingStatus(frame.worldMappingStatus)
     }
 }
@@ -805,6 +847,7 @@ extension GameViewController: GameManagerDelegate {
     
     func manager(_ manager: GameManager, joiningHost host: Player) {
         // host joined the game
+        // 主机加入游戏
         DispatchQueue.main.async {
             if self.sessionState == .waitingForBoard {
                 manager.send(boardAction: .requestBoardLocation)
@@ -819,6 +862,7 @@ extension GameViewController: GameManagerDelegate {
     
     func manager(_ manager: GameManager, joiningPlayer player: Player) {
         // non-host player joined the game
+        // 非主机玩家加入游戏
         guard !UserDefaults.standard.disableInGameUI else { return }
         
         DispatchQueue.main.async {
@@ -828,6 +872,7 @@ extension GameViewController: GameManagerDelegate {
 
         // If the gameplay music is already running, start it on the newly
         // connected client.
+        // 如果游戏音乐已经在播放,在新接入的客户端上播放.
         if musicCoordinator.currentMusicPlayer?.name == "music_gameplay" {
             let musicTime = musicCoordinator.currentMusicTime()
             os_log(.debug, "music play position = %f", musicTime)
@@ -839,10 +884,12 @@ extension GameViewController: GameManagerDelegate {
     
     func manager(_ manager: GameManager, leavingHost host: Player) {
         // host left the game
+        // 主机离开游戏
         guard !UserDefaults.standard.disableInGameUI else { return }
         
         DispatchQueue.main.async {
             // the game can no longer continue
+            // 游戏不能再存在了
             self.notificationLabel.text = "The host left the game. Please join another game or start your own!"
             self.notificationLabel.isHidden = false
         }
@@ -850,6 +897,7 @@ extension GameViewController: GameManagerDelegate {
     
     func manager(_ manager: GameManager, leavingPlayer player: Player) {
         // non-host player left the game
+        // 非主机玩家离开游戏
         guard !UserDefaults.standard.disableInGameUI else { return }
         
         DispatchQueue.main.async {
