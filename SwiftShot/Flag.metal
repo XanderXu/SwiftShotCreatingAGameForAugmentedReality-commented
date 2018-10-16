a2f9c6f6a97d4d979e4fe2dc475ccd4a94e01e11
XanderXu <517807635@qq.com>
2018-10-16 14:08:01 +0800

    metal 旗帜相关注释

--- SwiftShot/Flag.metal
+++ SwiftShot/Flag.metal
@@ -43,6 +43,7 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
+ 版权信息
  */
 
 #include <metal_stdlib>
@@ -56,6 +57,12 @@ using namespace metal;
  *
  * NOTE: statements involving fric have been
  * commented out on the assumption that fric is 1.!!!!
+ 
+ 一个点的权重总是 1.水平和竖直方向的弹性长度偏好也总是 1！！！！
+  弹性长度的公式：( force = strength * displacement )
+ 摩擦：( force = fric * vel )
+ 
+ 注意：涉及摩擦的陈述都是假设fric为1的情况下
  */
 //constant float fric = 1;
 constant float strength = 10;
@@ -65,6 +72,8 @@ constant float G = 0.025; // gravity
 
 // We set these as constants to give the compiler more opportunity to optimize.
 // Another choice would be to pass these values into the shader at runtime.
+// 我们将它们设置为常量，以便为编译器提供更多优化机会。
+// 另一种选择是在运行时将这些值传递到着色器。
 constant int WIDTH = 32;
 constant int HEIGHT = 20;
 
@@ -80,6 +89,7 @@ float3 GetVelocity(const device float3* velocities, uint x, uint y)
     return velocities[getIndex(x,y)];
 }
 
+// 定义的宏，以方便调用，当满足condition条件时，调用ApplyForce(inVertices, inVelocities, pos, vel, x, y, dx, dy, str)并累加到 force 上
 #define APPLY_FORCE(condition, dx, dy, str) \
 if(condition) \
 { \
@@ -97,26 +107,35 @@ float3 ApplyForce(const device float3* positions,
                   const float constraintStrength)
 {
     // Get the distance between the point and its neighbour of interest
+    // 获取点与其邻近点之间的距离（3D 空间中的距离，即变形程度向量）
     float3 deltaP = GetPosition(positions, x+dx, y+dy) - pos;
     
     float len = length(deltaP);
     
-    float nominalLen = sqrt( float(dx*dx+dy*dy) );
-    float sf = (len - nominalLen) * constraintStrength; // Spring force
+    float nominalLen = sqrt( float(dx*dx+dy*dy) );//名义上的距离（ 2D平面未拉伸扭转时的距离）
+    float sf = (len - nominalLen) * constraintStrength; // Spring force弹力
     
     // Get the velocity difference between the same two points:
+    // 获得相同两点之间的速度差异：
     float3 deltaV = GetVelocity(velocities, x+dx, y+dy) - vel;
     
-    float invLen = 1/len;
+    float invLen = 1/len;//距离的倒数，相当于除以距离（3D 空间的长度）
     
     // Dot that with the positional delta to get spring motion:
+    // 将其与位置偏移量点乘，得到弹性运动：
     float sv = dot(deltaP, deltaV) * invLen;
+    // 点乘又叫向量的内积、数量积，是一个向量和它在另一个向量上的投影的长度的乘积；是标量。
+    // 点乘反映着两个向量的“相似度”，两个向量越“相似”，它们的点乘越大。
     
+    
+    // 下面是计算力的公式：sf 是布料在 3D 空间变形产生的弹力；sv 是布料点运动速度及方向不同，产生的力；fric 是运动摩擦，这里值取 1；del/invLen 就是变形向量除以长度，即单位长度变形量（变形程度）
     // Force = -(sf + sv * fric) * del/invLen.
     sf += sv;    // friction = 1
     sf *= invLen;
     
     deltaP *= sf;
+    // 公式结束
+    
     
     return deltaP;
 }
@@ -137,12 +156,15 @@ kernel void updateVertex(const device float3* inVertices [[ buffer(0) ]],
     const uint y = id / WIDTH;
     
     // Point location, velocity, force (= acceleration, as mass is 1.).
+    // 点的位置，速度，力（因为质量是 1，所以加速度的值等于力的值）
     float3 pos = inVertices[id];
     float3 vel = inVelocities[id];
     
     // Start with 0 force.
+    // 从 0 的力开始
     float3 force(0,0,0);
     
+    // 累加各个方向的计算出来的力，到 force 上
     APPLY_FORCE(x < WIDTH-1,  1, 0, strength);
     APPLY_FORCE(x > 0,       -1, 0, strength);
     
@@ -159,41 +181,49 @@ kernel void updateVertex(const device float3* inVertices [[ buffer(0) ]],
     APPLY_FORCE(y > 1,        0, -2, strength2);
     
     // gravity
+    // 重力
     force.y += G;
     
     // Add wind contribution
+    // 添加风的贡献度
     float3 wind = simData.wind;
     
     if (x < WIDTH-1 && y < HEIGHT-1)
     {
+        // 注意此处的 right 和 up 是指向量叉乘的方向，决定了得到法线的正负（朝里面还是朝外面）
         float3 right = GetPosition(inVertices, x+1, y) - pos;
         float3 up = GetPosition(inVertices, x, y+1) - pos;
         float3 n = normalize(cross(right, up));
         
-        float3 relativeWind = wind - vel;
+        float3 relativeWind = wind - vel;//相对风速，即风实际速度与旗帜上某个点瞬时速度的差值
         
+        // 点乘又叫向量的内积、数量积，是一个向量和它在另一个向量上的投影的长度的乘积；是标量。
+        // 点乘反映着两个向量的“相似度”，两个向量越“相似”，它们的点乘越大。
         float f = dot(relativeWind, n) * 0.25;
         
         force += n*f;
     }
     
     // Pin the two left-most corners to the flagpole
+    // 将最左侧的两个角点钉在旗杆上
     if ((x == 0 && y == 0) || ((x == 0) && (y == HEIGHT-1)))
         force = 0;
     
     // Cancel force on the right edge of the flag
+    // 取消旗帜右边缘的力，不再计算
     if(x == WIDTH-1)
         force = 0;
     
     // move flag
-    float3 v = vel + (force*timestep);
+    // 移动旗帜
+    float3 v = vel + (force*timestep);//实际速度=原速度+（加速度*时间），force 在这里表示加速度，因为质量是 1，所以加速度===力
     outVelocities[id] = v;
     outVertices[id] = pos + v*timestep;
 }
 
 // This function computes the plane normals for the 6 planes touching the
 // vertex and averages them as the vertex normal
-
+// 该函数计算顶点周边 6 个平面的法线，并将其平均得到顶点法线
 kernel void updateNormal(const device float3* inVertices [[ buffer(0) ]],
                          device float3* outVertices [[ buffer(1) ]],
                          device float3* outNormals [[ buffer(2) ]],
@@ -205,10 +235,12 @@ kernel void updateNormal(const device float3* inVertices [[ buffer(0) ]],
     const float3 v1 = inVertices[id];
     
     float3 normal(0, 0, 0);
+    // 右侧的三个向量，组合求得三个法线
     if (x < WIDTH-1)
     {
         if (y < HEIGHT-1)
         {
+            // 注意此处的 right 和 up 是指向量叉乘的方向，决定了得到法线的正负（朝里面还是朝外面）
             float3 right = GetPosition(inVertices, x+1, y+1) - v1;
             float3 up = GetPosition(inVertices, x+1, y) - v1;
             float3 n = normalize(cross(right, up));
@@ -230,6 +262,8 @@ kernel void updateNormal(const device float3* inVertices [[ buffer(0) ]],
             normal += n;
         }
     }
+    
+    // 左侧的三个向量，组合求得三个法线
     if (x > 0)
     {
         if (y < HEIGHT-1)
@@ -277,6 +311,7 @@ kernel void smoothNormal(const device float3* inNormals [[ buffer(0) ]],
     
     float3 n(0,0,0);
     
+    // 将一个点与其周围 3 个点的法线相加，最后规范化法线 n，得到平滑后的法线
     for(int i = max(0, x - SMOOTH_FILTER_SIZE); i < min(WIDTH, x + SMOOTH_FILTER_SIZE); i++)
     {
         for(int j = max(0, y - SMOOTH_FILTER_SIZE); j < min(HEIGHT, y + SMOOTH_FILTER_SIZE); j++)
--- SwiftShot/Flag.swift
+++ SwiftShot/Flag.swift
@@ -59,9 +59,11 @@ class ClothSimMetalNode {
             }
         }
         
+        // 创建索引
         for y in 0..<(height - 1) {
             for x in 0..<(width - 1) {
                 // make 2 triangles from the 4 vertices of a quad
+                // 从四边形的 4 个顶点，构造出 2 个三角形。（i0是坐标(x,y)处的索引，i1 是右侧索引，i2 是下方点的索引，i3 是右下方的索引）
                 let i0 = y * width + x
                 let i1 = i0 + 1
                 let i2 = i0 + width
@@ -78,7 +80,7 @@ class ClothSimMetalNode {
                 indices.append(i1)
             }
         }
-        
+        // 顶点 Buffer 和顶点 GeometrySource，vertexBuffer2暂时为空
         let vertexBuffer1 = device.makeBuffer(bytes: vertices,
                                               length: vertices.count * MemoryLayout<float3>.size,
                                               options: [.cpuCacheModeWriteCombined])
@@ -93,6 +95,7 @@ class ClothSimMetalNode {
                                              dataOffset: 0,
                                              dataStride: MemoryLayout<float3>.size)
         
+        // 法线 Buffer 和法线 GeometrySource，normalWorkBuffer暂时为空
         let normalBuffer = device.makeBuffer(bytes: normals,
                                              length: normals.count * MemoryLayout<float3>.size,
                                              options: [.cpuCacheModeWriteCombined])
@@ -106,7 +109,7 @@ class ClothSimMetalNode {
                                              vertexCount: normals.count,
                                              dataOffset: 0,
                                              dataStride: MemoryLayout<float3>.size)
-        
+        // uv贴图 Buffer 和uv贴图 GeometrySource
         let uvBuffer = device.makeBuffer(bytes: uvs,
                                          length: uvs.count * MemoryLayout<float2>.size,
                                          options: [.cpuCacheModeWriteCombined])
@@ -118,16 +121,18 @@ class ClothSimMetalNode {
                                          dataOffset: 0,
                                          dataStride: MemoryLayout<float2>.size)
         
+        // 根据索引，及以上三种GeometrySource，创建几何体
         let indexElement = SCNGeometryElement(indices: indices, primitiveType: .triangles)
         let geo = SCNGeometry(sources: [vertexSource, normalSource, uvSource], elements: [indexElement])
         
         // velocity buffers
+        // 速度 buffer，暂时都为空
         let velocityBuffer1 = device.makeBuffer(length: vertices.count * MemoryLayout<float3>.size,
                                                 options: [.cpuCacheModeWriteCombined])
         
         let velocityBuffer2 = device.makeBuffer(length: vertices.count * MemoryLayout<float3>.size,
                                                 options: [.cpuCacheModeWriteCombined])
-        
+        // 保存
         self.geometry = geo
         self.vertexCount = vertices.count
         self.vb1 = vertexBuffer1!
@@ -141,6 +146,7 @@ class ClothSimMetalNode {
 /*
  Encapsulate the 'Metal stuff' within a single class to handle setup and execution
  of the compute shaders.
+ 将'Metal stuff'封装在一个类中以处理设置和执行计算着色器。
  */
 class MetalClothSimulator {
     let device: MTLDevice
@@ -164,11 +170,13 @@ class MetalClothSimulator {
         
         commandQueue = device.makeCommandQueue()!
         
+        // 读取加载 Flag.metal 中的三个着色器
         defaultLibrary = device.makeDefaultLibrary()!
         functionClothSim = defaultLibrary.makeFunction(name: "updateVertex")!
         functionNormalUpdate = defaultLibrary.makeFunction(name: "updateNormal")!
         functionNormalSmooth = defaultLibrary.makeFunction(name: "smoothNormal")!
 
+        // 创建计算管线
         do {
             pipelineStateClothSim = try device.makeComputePipelineState(function: functionClothSim)
             pipelineStateNormalUpdate = try device.makeComputePipelineState(function: functionNormalUpdate)
@@ -177,13 +185,15 @@ class MetalClothSimulator {
             fatalError("\(error)")
         }
     }
-    
+    /// 从一个 Node 创建旗帜模拟
     func createFlagSimulationFromNode(_ node: SCNNode) {
+        // 创建指定尺寸的风格数据，并构建成 clothNode
         let meshData = ClothSimMetalNode(device: device, width: width, height: height)
         let clothNode = SCNNode(geometry: meshData.geometry)
         
         guard let flag = node.childNode(withName: "flagStaticWave", recursively: true) else { return }
         
+        // 处理缩放和旋转，材质
         let boundingBox = flag.simdBoundingBox
         let existingFlagBV = boundingBox.max - boundingBox.min
         let rescaleToMatchSizeMatrix = float4x4(scale: existingFlagBV.x / Float(width))
@@ -196,14 +206,18 @@ class MetalClothSimulator {
         clothNode.geometry?.firstMaterial = flag.geometry?.firstMaterial
         clothNode.geometry?.firstMaterial?.isDoubleSided = true
 
+        // 将 flag 节点替换为 clothNode
         flag.parent?.replaceChildNode(flag, with: clothNode)
-        clothNode.setupPaintColorMask(clothNode.geometry!, name: "flag_flagA")
+        
+        // 设置材质，颜色，并修复法线贴图
+        clothNode.setupPaintColorMask(clothNode.geometry!, name: "flag_flagA")//内部通过一个 shader 实现
         clothNode.setPaintColors()
         clothNode.fixNormalMaps()
         
         clothData.append( ClothData(clothNode: clothNode, meshData: meshData) )
     }
     
+    // 更新，计算风的效果
     func update(_ node: SCNNode) {
        
         for cloth in clothData {
@@ -211,15 +225,18 @@ class MetalClothSimulator {
             
             // The multiplier is to rescale ball to flag model space.
             // The correct value should be passed in.
+            // 这个乘数的作用是将小球重缩放到旗帜模型空间。应传入正确的值。
             let simData = SimulationData(wind: wind)
             
             deform(cloth.meshData, simData: simData)
         }
     }
 
+    // 将数值传入给管线对应的 shader 中
     func deform(_ mesh: ClothSimMetalNode, simData: SimulationData) {
         var simData = simData
         
+        // 设置布料模拟的管线状态
         let w = pipelineStateClothSim.threadExecutionWidth
         let threadsPerThreadgroup = MTLSizeMake(w, 1, 1)
         
@@ -232,19 +249,23 @@ class MetalClothSimulator {
         
         clothSimCommandEncoder?.setComputePipelineState(pipelineStateClothSim)
         
+        
+        // 设置各种 buffer
         clothSimCommandEncoder?.setBuffer(mesh.vb1, offset: 0, index: 0)
         clothSimCommandEncoder?.setBuffer(mesh.vb2, offset: 0, index: 1)
         clothSimCommandEncoder?.setBuffer(mesh.velocityBuffers[mesh.currentBufferIndex], offset: 0, index: 2)
         mesh.currentBufferIndex = (mesh.currentBufferIndex + 1) % 2
         clothSimCommandEncoder?.setBuffer(mesh.velocityBuffers[mesh.currentBufferIndex], offset: 0, index: 3)
         clothSimCommandEncoder?.setBytes(&simData, length: MemoryLayout<SimulationData>.size, index: 4)
-        
+        // 线程
         clothSimCommandEncoder?.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
         
+        // 提交
         clothSimCommandEncoder?.endEncoding()
         clothSimCommandBuffer?.commit()
         
-        //
+        
+        // 同上，设置法线的计算管线
         
         let normalComputeCommandBuffer = commandQueue.makeCommandBuffer()
         let normalComputeCommandEncoder = normalComputeCommandBuffer?.makeComputeCommandEncoder()
@@ -258,7 +279,7 @@ class MetalClothSimulator {
         normalComputeCommandEncoder?.endEncoding()
         normalComputeCommandBuffer?.commit()
 
-        //
+        //同上，设置平滑过的法线计算管线
         
         let normalSmoothComputeCommandBuffer = commandQueue.makeCommandBuffer()
         let normalSmoothComputeCommandEncoder = normalSmoothComputeCommandBuffer?.makeComputeCommandEncoder()
